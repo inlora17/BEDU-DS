3ea80fa4ac12a0504e7696e6ada94b556d927e44
inlora17 <72327928+inlora17@users.noreply.github.com>
2020-10-29 18:16:31 -0600

    Add files via upload

---
+++ Retos Sesión 1.sql
@@ -0,0 +1,31 @@
+#RETO 1
+----- Tipos de datos
+# int
+#varchar
+#double
+#timestamp
+----------------
+#RETO 2
+-- ¿Cuál es el nombre de los empleados con el puesto 4?
+SELECT nombre
+FROM empleado
+WHERE id_puesto = 4;
+-- ¿Qué puestos tienen un salario mayor a $10,000? 
+SELECT*
+FROM puesto 
+WHERE salario > 10000;
+-- ¿Qué articulos tienen un precio mayor a $1,000 y un iva mayor a 100?
+SELECT *
+FROM articulo
+WHERE precio > 1000 AND iva > 100;
+-- ¿Qué ventas incluyen los artículo 135 o 963 y fueron hechas por los empleados 835 o 369?
+SELECT *
+FROM venta
+WHERE id_articulo IN (135, 963) AND id_empleado IN (835, 369);
+
+#RETO 3
+-- top 5 puestos por salario
+SELECT *
+FROM puesto
+ORDER BY salario DESC
+LIMIT 5;
